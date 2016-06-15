8e593f134c20a619383d71fc5d829db5520dbdb5
James Lee <jameslee@jamess-mac.local>
2016-06-15 20:28:08 +0800

    player dictionary finished

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,9 +1,23 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
-
-//James's comment
-
-//Second comment
+var players = [["Name":"Joe Smith","Height":42.0,"Soccer Experience": true, "Guardian Name": "Jim and Jan Smith"],
+    ["Name":"Jill Tanner","Height":36.0,"Soccer Experience":true,"Guardian Name": "Clara Tanner"],
+    ["Name": "Bill Bon", "Height":43.0, "Soccer Experience":true,"Guardian Name":"Sara and Jenny Bon"],
+    ["Name": "Eva Gordon", "Height":45.0, "Soccer Experience":false, "Guardian Name":"Wendy and Mike Gordon"],
+    ["Name":"Matt Gill","Height":40.0, "Soccer Experience":false, "Guardian Name":"Charles and Sylvia Gill"],
+    ["Name":"Kimmy Stein", "Height":41.0, "Soccer Experience":false,"Guardian Name":"Bill and Hillary Stein"],
+    ["Name":"Sammy Adams", "Height":45.0, "Soccer Experience":false,"Guardian Name":"Jeff Adams"],
+    ["Name":"Karl Saygan", "Height":42.0, "Soccer Experience":true, "Guardian Name":"Heather Bledsoe"],
+    ["Name":"Suzane Greenberg", "Height":44.0, "Soccer Experience":true, "Guardian Name":"Henrietta Dumas"],
+    ["Name":"Sal Dali","Height":41.0, "Soccer Experience":false,"Guardian Name":"Gala Dali"],
+    ["Name":"Joe Kavalier","Height":39.0,"Soccer Experience" :false,"Guardian Name":"Sam and Elaine Kavalier"],
+    ["Name":"Ben Finkelstein","Height":44.0,"Soccer Experience":false,"Guardian Name":"Aaron and Jill Finkelstein"],
+    ["Name":"Diego Soto","Height":41.0,"Soccer Experience":true,"Guardian Name":"Robin and Sarika Soto"],
+    ["Name":"Chloe Alaska","Height":47.0,"Soccer Experience":false,"Guardian Name":"David and Jamie Alaska"],
+    ["Name":"Arnold Willis","Height":43.0,"Soccer Experience":false,"Guardian Name":"Claire Willis"],
+    ["Name":"Phillip Helm","Height":44.0,"Soccer Experience":true,"Guardian Name":"Thomas Helm and Eva Jones"],
+    ["Name":"Les Clay","Height":42.0,"Soccer Experience":true,"Guardian Name":"Wynonna Brown"],
+    ["Name":"Herschel Krustofski","Height":45.0,"Soccer Experience":true,"Guardian Name":"Hyman and Rachel Krustofski"]
+]
